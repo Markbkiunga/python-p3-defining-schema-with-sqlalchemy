1723bf75cd3b351248dc62325fede8d4b936e9fb
Mark Brian <markbkiunga2017@gmail.com>
2024-10-07 12:55:54 +0300

    Completes codealong

--- lib/debug.py
+++ lib/debug.py
@@ -2,9 +2,9 @@
 
 from sqlalchemy import create_engine
 
-from sqlalchemy_sandbox import Student
+from sqlalchemy_sandbox import Base, Student
 
-engine = create_engine('sqlite:///students.db')
 
-if __name__ == '__main__':
-    import ipdb; ipdb.set_trace()
+if __name__ == "__main__":
+    engine = create_engine("sqlite:///students.db")
+    Base.metadata.create_all(engine)
--- lib/sqlalchemy_sandbox.py
+++ lib/sqlalchemy_sandbox.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -8,11 +6,11 @@ Base = declarative_base()
 
 class Student(Base):
     __tablename__ = "students"
+
     id = Column(Integer(), primary_key=True)
     name = Column(String())
 
 
 if __name__ == "__main__":
-    pass
     engine = create_engine("sqlite:///students.db")
     Base.metadata.create_all(engine)
